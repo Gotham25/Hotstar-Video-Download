24b918b840966d392bb138d941d9a1e24ffc0e2b
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2018-10-19 12:41:08 +0530

    Fixed short_open_tag issue
    Former-commit-id: 0da8cd76d63d5344beb3feb8bf83aeaeec09d957

--- installYdlBinaries.php
+++ installYdlBinaries.php
@@ -1,4 +1,4 @@
-<?
+<?php
 	$ydlReleaseUrl = "https://github.com/rg3/youtube-dl/releases/latest";
 	$ydlDownloadPath = str_ireplace("tag", "download", shell_exec("curl --silent ".$ydlReleaseUrl." | grep -Po '(?<=href=\")[^\"]*' | tr -d '\n'") )."/youtube-dl";
 	echo "Downloading youtube-dl binaries...";
