4dfbe7572b07ef29adf6e02fc5ddd3ea96772ab6
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-02-25 14:34:54 +0530

    Added files for Google Drive upload functionality and domain verification

---
+++ UploadToGoogleDrive.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST)){
+	$authCode = urldecode($_POST["authCode"]);
+	$videoFileName = $_POST["fileName"];
+	$gdriveUploadCommand = "php gdriveUpload.php ".$authCode." ".$videoFileName;
+	
+	$gdriveUploadOutput = shell_exec($gdriveUploadCommand);
+	
+	echo $gdriveUploadOutput;
+}else{
+	die("Invalid script invocation. Error : No POST data given");
+}
+
+?>
