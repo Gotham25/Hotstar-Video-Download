b1a9677362dfe11050e9292e2e8565adc46bf26a
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-03 21:54:04 +0530

    Replaced curl PHP request with Guzzle http and moved m3u8 parsing to separate function in separate file
    Former-commit-id: f61ff2422665fe700133d791f3d6b60ab90eb992

---
+++ src/helper/utils.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once( realpath( dirname(__FILE__) . '/../..') . "/vendor/autoload.php" );
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ClientException;
+
+function make_get_request($url, array $headers = []) {
+   
+   $guzzleClient = new GuzzleHttp\Client();
+   try {
+      $guzzleResponse  = $guzzleClient->request('GET', $url, [
+            'headers' => $headers
+         ]);
+      $guzzleResponseCode = $guzzleResponse->getStatusCode();
+      
+      if($guzzleResponseCode == 200){
+         $guzzleResponseBody = (string)$guzzleResponse->getBody();
+         return $guzzleResponseBody;
+      }
+      
+      throw new ClientException("Error code $guzzleResponseCode. Error Processing Request Invalid response.", 1);
+      
+   } catch(ConnectException $e) {
+      return $e->getMessage();
+   } catch(ClientException $e) {
+      $guzzleErrorResponse = $e->getResponse();
+      $guzzleErrorResponseBody = $guzzleErrorResponse->getBody()->getContents();
+      return $guzzleErrorResponseBody;
+   }
+}
+
+function startsWith($haystack, $needle) {
+	$length = strlen($needle);
+	return (substr($haystack, 0, $length) === $needle);
+}
+
+function getKForm($num) {
+
+   if(!is_integer($num)){
+      throw new InvalidArgumentException("Invalid parameter type passed. Parameter should be integer");
+   }
+
+	if($num < 1000) {
+		return $num;
+	}
+	return intval($num/1000);
+}
+
+?>
