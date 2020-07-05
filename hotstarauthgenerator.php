86b980ffd8dd3b26a0125ef00a67140a059ec974
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2020-07-05 08:05:34 +0530

    Update hotstarauthgenerator.php

--- hotstarauthgenerator.php
+++ hotstarauthgenerator.php
@@ -1,7 +1,6 @@
 <?php
 
 error_log(json_encode($_SERVER));
-header_remove('Access-Control-Allow-Origin');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   //header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
